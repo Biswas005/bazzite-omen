31f6ffc387e4d24c973693301cc3c4fb7c609614
Amardeep Biswas <58628428+Biswas005@users.noreply.github.com>
2025-06-25 13:00:13 +0530

    Update hp-wmi.c
    test 2 max outing the combined powers work but the cpu ppt values doesnot .

--- build_files/hp-wmi.c
+++ build_files/hp-wmi.c
@@ -1303,9 +1303,9 @@ static const struct omen_power_profile omen_profiles[] = {
     // Cool
     { 28, 30, 35,  40,   0, 0, 0, 75},
     // Balanced
-    { 35, 35, 55, 125,   1, 0, 0, 80},
+    { 35, 35, 65, 125,   1, 0, 0, 80},
     // Performance (max everything)
-    { 0, 0, 0, 0,   1, 1, 1, 87},
+    { 0, 0, 100, 0,   1, 1, 1, 87},
 };
 
 static int platform_profile_omen_get_ec(enum platform_profile_option *profile)
